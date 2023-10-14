1b3f95b2fdecb2dc67138a7bd38ca0d0d977466b
Joshua Silveous <JoshSilveous@gmail.com>
2023-10-14 17:35:09 -0400

    began home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import { Home } from './components/Home'
 import './App.scss'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import { Login } from './components/Login'
-import { CreateAccount } from './components/CreateAccount'
+import { Login } from './components/Authentication/Login'
+import { CreateAccount } from './components/Authentication/CreateAccount'
 
 function App() {
 	const [sessionInfo, setSessionInfo] = useState({ username: '', token: '' })
-	const needsLogin = sessionInfo.username === '' && sessionInfo.token === ''
-	if (needsLogin) {
+	if (!sessionInfo.token) {
 		console.log(window.location.pathname)
 		if (window.location.pathname !== '/authentication/login') {
 			window.location.pathname = 'authentication/login'
@@ -17,14 +16,19 @@ function App() {
 	return (
 		<Router>
 			<h1>Money Tracker!</h1>
-			<Routes>
-				<Route path='/authentication/login' element={<Login />} />
-				<Route
-					path='/authentication/create-account'
-					element={<CreateAccount />}
-				/>
-				<Route path='/' element={<Home />} />
-			</Routes>
+			{!sessionInfo.token && (
+				<Routes>
+					<Route
+						path='/authentication/login'
+						element={<Login setSessionInfo={setSessionInfo} />}
+					/>
+					<Route
+						path='/authentication/create-account'
+						element={<CreateAccount />}
+					/>
+				</Routes>
+			)}
+			{sessionInfo.token && <Home sessionInfo={sessionInfo} />}
 		</Router>
 	)
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,14 @@
-export function Home() {
-    return <>
-        <h2>Home! This must mean you're logged in!</h2>
-    </>
+interface HomeProps {
+	sessionInfo: { username: string; token: string }
+}
+export function Home({ sessionInfo }: HomeProps) {
+	return (
+		<>
+			<h2>Home! This must mean you're logged in!</h2>
+			<p>Username: {sessionInfo.username}</p>
+			<p style={{ maxWidth: '500px', wordWrap: 'break-word' }}>
+				Token: {sessionInfo.token}
+			</p>
+		</>
+	)
 }
