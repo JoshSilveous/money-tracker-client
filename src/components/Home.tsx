d956660ad8203a1b2986845cb72bccc8b3090e24
Joshua Silveous <JoshSilveous@gmail.com>
2023-10-14 19:28:10 -0400

    added popup trigger for testing

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,14 @@
+import React from 'react'
+import { triggerPopup } from '../popup/popup'
+import { NewCategory } from './Popups/NewCategory'
 interface HomeProps {
 	sessionInfo: { username: string; token: string }
 }
 export function Home({ sessionInfo }: HomeProps) {
+	function buttonHandle() {
+		triggerPopup(<NewCategory sessionInfo={sessionInfo} />)
+	}
+
 	return (
 		<>
 			<h2>Home! This must mean you're logged in!</h2>
@@ -9,6 +16,7 @@ export function Home({ sessionInfo }: HomeProps) {
 			<p style={{ maxWidth: '500px', wordWrap: 'break-word' }}>
 				Token: {sessionInfo.token}
 			</p>
+			<button onClick={buttonHandle}>Popup!</button>
 		</>
 	)
 }
