fb4e1778abac758934a363270d834288edc5d012
Joshua Silveous <JoshSilveous@gmail.com>
2023-10-27 11:39:44 -0400

    added display transactions for development

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -18,6 +18,7 @@ export function Home({ context }: HomeProps) {
 	}
 	const [devCatsList, setDevCatsList] = React.useState()
 	const [devActsList, setDevActsList] = React.useState()
+	const [devTransactionList, setDevTransactionList] = React.useState()
 
 	// (dev only)
 	interface Category {
@@ -27,6 +28,14 @@ export function Home({ context }: HomeProps) {
 	interface Account {
 		account_id: number
 		name: string
+	}
+	interface Transaction {
+		transaction_id: number
+		name: string
+		timestamp: string
+		amount: number
+		category_name: string
+		account_name: string
 	}
 	function devGetCategories(): Category[] | void {
 		const apiUrl = 'http://localhost:3000/api/getallcategories'
@@ -116,6 +125,83 @@ export function Home({ context }: HomeProps) {
 				console.error(err)
 			})
 	}
+
+	function devGetDisplayData(): Transaction[] | void {
+		const apiUrl = 'http://localhost:3000/api/getdisplaydata'
+
+		/* 
+		Example Data Structure
+		{
+			username: newUserCredentials.username,
+			token: token,
+			payload: {
+				resPerPage: 10,
+				thisPage: 1,
+				orderBy: 'timestamp',
+				orderByDirection: 'DESC',
+			},
+		}
+		*/
+		const data = {
+			username: context.username,
+			token: context.token,
+			payload: {
+				resPerPage: 10,
+				thisPage: 1,
+				orderBy: 'timestamp',
+				orderByDirection: 'DESC',
+			},
+		}
+		const headers = {
+			'Content-Type': 'application/json',
+		}
+		const requestOptions = {
+			method: 'POST',
+			headers,
+			body: JSON.stringify(data),
+		}
+		fetch(apiUrl, requestOptions)
+			.then((res) => {
+				console.log('res recieved:', res)
+				if (res.ok) {
+					return res.json()
+				} else {
+					throw new Error(res.statusText)
+				}
+			})
+			.then((data) => {
+				setDevTransactionList(
+					data.displayData.map((transaction: Transaction) => {
+						return (
+							<tr>
+								<th style={{ width: '15px' }}>
+									{transaction.transaction_id}
+								</th>
+								<td style={{ width: '100px' }}>
+									{transaction.name}
+								</td>
+								<td style={{ width: '60px' }}>
+									{transaction.timestamp}
+								</td>
+								<td style={{ width: '30px' }}>
+									{transaction.amount}
+								</td>
+								<td style={{ width: '80px' }}>
+									{transaction.category_name}
+								</td>
+								<td style={{ width: '80px' }}>
+									{transaction.account_name}
+								</td>
+							</tr>
+						)
+					})
+				)
+			})
+			.catch((err) => {
+				console.log('error while running dev command getAccounts')
+				console.error(err)
+			})
+	}
 	React.useEffect(() => {
 		devGetCategories()
 		devGetAccounts()
@@ -143,16 +229,56 @@ export function Home({ context }: HomeProps) {
 				<table
 					style={{
 						backgroundColor: 'var(--input_border_color)',
+						textAlign: 'left',
+						paddingLeft: '10px',
 					}}
 				>
+					<tr>
+						<th>category_id</th>
+						<th>name</th>
+					</tr>
 					{devCatsList}
 				</table>
 			</div>
 			<div style={{ display: 'flex', flexDirection: 'column' }}>
 				<button onClick={devGetAccounts}>Update Account List</button>
 
-				<table style={{ backgroundColor: 'var(--input_border_color)' }}>
+				<table
+					style={{
+						backgroundColor: 'var(--input_border_color)',
+						textAlign: 'left',
+						paddingLeft: '10px',
+					}}
+				>
+					<tr>
+						<th>account_id</th>
+						<th>name</th>
+					</tr>
 					{devActsList}
+				</table>
+			</div>
+			<div style={{ display: 'flex', flexDirection: 'column' }}>
+				<button onClick={devGetDisplayData}>
+					Update Transaction List
+				</button>
+
+				<table
+					style={{
+						backgroundColor: 'var(--input_border_color)',
+						textAlign: 'left',
+						paddingLeft: '10px',
+						width: '1000px',
+					}}
+				>
+					<tr>
+						<th>transaction_id</th>
+						<th>name</th>
+						<th>timestamp</th>
+						<th>amount</th>
+						<th>category_name</th>
+						<th>account_name</th>
+					</tr>
+					{devTransactionList}
 				</table>
 			</div>
 		</>
